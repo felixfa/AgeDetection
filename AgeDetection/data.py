d42f79960c0763e8752f9a3b0ee62b4d54df4823
felix faehnrich <felix@faehnrich.de>
2021-06-05 13:27:49 +0200

    Download and Unzip it, if the folder is not already there

---
+++ AgeDetection/data.py
@@ -0,0 +1,28 @@
+from google.cloud import storage
+from zipfile import ZipFile
+from os import path
+
+BUCKET_NAME="age_detection_faehnrich"
+STORAGE_LOCATION = 'raw_data'
+
+def download():
+    client = storage.Client().bucket(BUCKET_NAME)
+    blob = client.blob(STORAGE_LOCATION + "/UTKFace.zip")
+    print("Downloading...")
+    blob.download_to_filename("../raw_data/Faces.zip")
+    print("Downloading Done!")
+
+def unzip():
+    print("Unzipping...")
+    with ZipFile('../raw_data/Faces.zip', 'r') as zip_ref:
+        zip_ref.extractall("../raw_data/")
+    print('Unzipping Done!')
+
+
+def get_data():
+    if path.exists('../raw_data/Faces') == False:
+        download()
+        unzip()
+
+if __name__ == '__main__':
+    get_data()
