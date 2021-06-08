bb71e87cba5af52efc2d569ce8b09290a73bf39e
felix faehnrich <felix@faehnrich.de>
2021-06-08 17:50:41 +0200

    Predict examples

--- AgeDetection/predict.py
+++ AgeDetection/predict.py
@@ -1,6 +1,5 @@
 from tensorflow.keras import models
-from os import path
-from utils import load_images_from_folder, convert_number
+from utils import convert_number
 import numpy as np
 from PIL import Image
 from autocrop import Cropper
@@ -9,35 +8,41 @@ import cv2
 
 def loading_the_model():
     # load the model from disk
-    if path.exists('../models/best_model2') == False:
-        print('TO DO')
-        #TO DO
-    print('Nothing to Download!')
-    model = models.load_model('../models/best_model2')
+    model = models.load_model('../models/best_model')
     return model
 
-def loading_one_image():
+def loading_images():
     cropper = Cropper(width=100,height=100)
-
-    # Loading one picture
-    #cropped_array = cropper.crop('../test_data/IMG_2278.JPG') #Lesly
-    #cropped_array = cropper.crop('../test_data/IMG_2278.JPG') #Felix
-    #cropped_array = cropper.crop('../test_data/0d33a016-7cdb-4184-938a-ffae451a7eda.JPG') #Rami
-    cropped_array = cropper.crop('../test_data/Bildschirmfoto 2021-06-05 um 16.30.02.png') #Tiago
-
-    cropped_array = np.expand_dims(cropped_array,axis=0)
+    cropped_array = []
+    # Loading images
+    #cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Lesly
+    #cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Felix
+    #cropped_array.append(cropper.crop('../test_data/0d33a016-7cdb-4184-938a-ffae451a7eda.JPG')) #Rami
+    #cropped_array.append(cropper.crop('../test_data/Bildschirmfoto 2021-06-05 um 16.30.02.png')) #Tiago
+    cropped_array.append(cropper.crop('../test_data/Qiwei.png')) #Qiwei
+    #cropped_array.append(cropper.crop('../test_data/Nicole.jpg')) #Nicole
+    #cropped_array.append(cropper.crop('../test_data/perry.png')) #Matthew Perry
+    cropped_array = np.array(cropped_array)
+    cropped_array = cropped_array / 255 - 0.5
+    #cropped_array = np.expand_dims(cropped_array,axis=0)
     return cropped_array
+
 
 def predict(model,X):
     y_pred = model.predict(X)
-    y_pred = convert_number(int(np.argsort(y_pred[0])[-1]))
     return y_pred
 
 if __name__ == '__main__':
     model = loading_the_model()
-    X = loading_one_image()
+    X = loading_images()
     y_pred = predict(model,X)
-    print(f'Prediction: {y_pred}')
+    print(f'Prediction_1: {convert_number(int(np.argsort(y_pred[0])[-1]))}')
+    print(f'Age Bin_1:  {int(np.argsort(y_pred[0])[-1])}')
+    print(f'Prediction_2: {convert_number(int(np.argsort(y_pred[0])[-2]))}')
+    print(f'Age Bin_2:  {int(np.argsort(y_pred[0])[-2])}')
+    print(f'Prediction_3: {convert_number(int(np.argsort(y_pred[0])[-3]))}')
+    print(f'Age Bin_3:  {int(np.argsort(y_pred[0])[-3])}')
+    print(f'Propability: {y_pred}')
 
 
 
