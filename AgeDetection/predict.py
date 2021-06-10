3bf2444f69382ac53344e865495a11bcfe66c554
Tiago Pereira <targpereira@gmail.com>
2021-06-10 16:03:40 +0100

    Updating API

--- AgeDetection/predict.py
+++ AgeDetection/predict.py
@@ -11,24 +11,25 @@ def loading_the_model():
     model = models.load_model('../models/best_model')
     return model
 
+
 def loading_images():
-    cropper = Cropper(width=100,height=100)
+    cropper = Cropper(width=100, height=100)
     cropped_array = []
     # Loading images
-    #cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Lesly
-    #cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Felix
-    #cropped_array.append(cropper.crop('../test_data/0d33a016-7cdb-4184-938a-ffae451a7eda.JPG')) #Rami
-    #cropped_array.append(cropper.crop('../test_data/Bildschirmfoto 2021-06-05 um 16.30.02.png')) #Tiago
-    cropped_array.append(cropper.crop('../test_data/Qiwei.png')) #Qiwei
-    #cropped_array.append(cropper.crop('../test_data/Nicole.jpg')) #Nicole
-    #cropped_array.append(cropper.crop('../test_data/perry.png')) #Matthew Perry
+    # cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Lesly
+    # cropped_array.append(cropper.crop('../test_data/IMG_2278.JPG')) #Felix
+    # cropped_array.append(cropper.crop('../test_data/0d33a016-7cdb-4184-938a-ffae451a7eda.JPG')) #Rami
+    # cropped_array.append(cropper.crop('../test_data/Bildschirmfoto 2021-06-05 um 16.30.02.png')) #Tiago
+    cropped_array.append(cropper.crop('../test_data/Qiwei.png'))  # Qiwei
+    # cropped_array.append(cropper.crop('../test_data/Nicole.jpg')) #Nicole
+    # cropped_array.append(cropper.crop('../test_data/perry.png')) #Matthew Perry
     cropped_array = np.array(cropped_array)
     cropped_array = cropped_array / 255 - 0.5
-    #cropped_array = np.expand_dims(cropped_array,axis=0)
+    # cropped_array = np.expand_dims(cropped_array,axis=0)
     return cropped_array
 
 
-def predict(model,X):
+def predict(model, X):
     y_pred = model.predict(X)
     return y_pred
 
@@ -36,7 +37,7 @@ def predict(model,X):
 if __name__ == '__main__':
     model = loading_the_model()
     X = loading_images()
-    y_pred = predict(model,X)
+    y_pred = predict(model, X)
     print(f'Prediction_1: {convert_number(int(np.argsort(y_pred[0])[-1]))}')
     print(f'Age Bin_1:  {int(np.argsort(y_pred[0])[-1])}')
     print(f'Prediction_2: {convert_number(int(np.argsort(y_pred[0])[-2]))}')
@@ -44,6 +45,3 @@ if __name__ == '__main__':
     print(f'Prediction_3: {convert_number(int(np.argsort(y_pred[0])[-3]))}')
     print(f'Age Bin_3:  {int(np.argsort(y_pred[0])[-3])}')
     print(f'Propability: {y_pred}')
-
-
-
--- api/fast.py
+++ api/fast.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
 from autocrop import Cropper
 from tensorflow.keras import models
 import numpy as np
@@ -16,18 +15,18 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
-model_path = '/models/best_model'
+model_path = '/home/fruntxas/code/felixfa/AgeDetection/models/best_model'
 
 model = models.load_model(model_path)
 
 
-def predict(model,X):
+def predict(model, X):
     y_pred = model.predict(X)
     return y_pred
 
 
 def load_image_into_numpy_array(image):
-    cropper = Cropper(width=100, height=100)    
+    cropper = Cropper(width=100, height=100)
     cropped_array = cropper.crop(image)
     return np.expand_dims(cropped_array, axis=0)
 
@@ -39,15 +38,15 @@ def convert_number(num):
 # Upload Image
 @app.post("/image")
 async def read_root(file: UploadFile = File(...)):
-    
-    with open("tmp.png","wb+") as data:
+
+    with open("tmp.png", "wb+") as data:
         data.write(file.file.read())
         print("Written to image")
 
     X = load_image_into_numpy_array("tmp.png")
-    # X = X/255 - 0.5
+    X = X/255 - 0.5
     print("Image converted to array")
-    y_pred = predict(model,X)
+    y_pred = predict(model, X)
     print(y_pred)
     print(type(y_pred))
     guess = convert_number(int(np.argsort(y_pred[0])[-1]))
